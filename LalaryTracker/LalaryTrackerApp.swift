4c64f431948a2be6d4ac60111c8e9d5ec3dd4858
eroccked <eroccked@gmail.com>
2025-10-30 16:34:19 +0200

    Initial Commit

---
+++ LalaryTracker/LalaryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LalaryTrackerApp.swift
+//  LalaryTracker
+//
+//  Created by Taras Buhra on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct LalaryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
